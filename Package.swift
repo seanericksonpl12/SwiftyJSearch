77ee1695daececdf00bd6ec56ab2f8423e509af0
seanericksonpl12 <seanericksonpl12@gmail.com>
2023-08-28 08:27:23 -0500

    Created bfs search

--- Package.swift
+++ Package.swift
@@ -6,21 +6,18 @@ import PackageDescription
 let package = Package(
     name: "SwiftyJSearch",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftyJSearch",
             targets: ["SwiftyJSearch"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyJSearch",
-            dependencies: []),
+            dependencies: ["SwiftyJSON"],
+            path: "Sources"),
         .testTarget(
             name: "SwiftyJSearchTests",
             dependencies: ["SwiftyJSearch"]),
--- Tests/SwiftyJSearchTests/SwiftyJSearchTests.swift
+++ Tests/SwiftyJSearchTests/SwiftyJSearchTests.swift
@@ -1,11 +1,45 @@
 import XCTest
+import SwiftyJSON
 @testable import SwiftyJSearch
 
 final class SwiftyJSearchTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftyJSearch().text, "Hello, World!")
+    
+    var family: JSON!
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        self.family = [
+            "names": [
+                "Brooke Abigail Matos",
+                "Rowan Danger Matos"
+            ],
+            "motto": "Hey, I don't know about you, but I'm feeling twenty-two! So, release the KrakenDev!"
+        ]
+    }
+    
+    func testBfs() {
+        let nestedFamily: JSON = [
+            "count": 1,
+            "families": [
+                [
+                    "isACoolFamily": true,
+                    "family": [
+                        "hello": family
+                    ]
+                ],
+                [
+                    "second list": true,
+                    "has more stuff": true,
+                    "second family": [
+                        "names": [
+                            "Sean Erickson"
+                        ],
+                        "motto": "Hey, I don't know about you, but I'm feeling twenty-two! So, release the KrakenDev!"
+                    ]
+                ]
+            ]
+        ]
+        print(try? nestedFamily.bfs(for: "names", returning: .first))
     }
 }
